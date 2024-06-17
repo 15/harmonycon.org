f02b76f7bae683c7ec00f3b2b5cf5a1cf7ac184b
15 <15@soiree.horse>
2024-06-17 01:37:29 -0700

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import swup from '@swup/astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://harmonycon.org",
+  prefetch: {
+    prefetchAll: true
+  },
+  integrations: [tailwind(), swup({
+    containers: ['#swup'],
+    theme: ['overlay', { direction: 'to-bottom'}]
+  })]
+});
