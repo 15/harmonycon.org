b3f2b40fb13ac6b4219cda0a934a1d6e8d633a9a
15 <15@soiree.horse>
2024-07-19 13:23:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,18 +12,23 @@ export default defineConfig({
   },
   integrations: [
     tailwind(),
-    // swup({
-    //   containers: ["#swup"],
-    //   theme: [
-    //     "overlay",
-    //     {
-    //       direction: "to-bottom",
-    //     },
-    //   ],
-    //   globalInstance: true,
-    //   smoothScrolling: false,
-    //   // animateHistoryBrowsing: true,
-    // }),
+    swup({
+      containers: ["#swup"],
+      theme: [
+        "overlay",
+        {
+          direction: "to-bottom",
+        },
+      ],
+      cache: false,
+      preload: {
+        hover: true,
+        visible: true,
+      },
+      globalInstance: true,
+      smoothScrolling: false,
+      // animateHistoryBrowsing: true,
+    }),
     icon(),
   ],
 });
