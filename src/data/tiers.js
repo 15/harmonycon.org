27e8daff6630b8b4db83c1d157bd4c92d60b6ebd
15 <146901496+15@users.noreply.github.com>
2024-08-07 23:41:26 -0700

    Mobile styling for registration page

--- src/data/tiers.js
+++ src/data/tiers.js
@@ -1,27 +1,27 @@
 export default [
   {
-    name: `<div class="text-2xl"><span class="italic">Filly Elliot</span> - Child (Age 3&ndash;12)</div>`,
+    name: `<div class="text-xl xl:text-2xl"><span class="italic">Filly Elliot</span> - Child (Age 3&ndash;12)</div>`,
     perks: ["Badge", "Conbook", "Simple colored lanyard"],
-    price: "<span class='text-2xl'>$20</span>",
+    price: "<span class='text-xl xl:text-2xl'>$20</span>",
   },
   {
-    name: `<div class="text-2xl"><span class="italic">Hoofloose</span> - Teen (Age 13&ndash;17)</div>`,
+    name: `<div class="text-xl xl:text-2xl"><span class="italic">Hoofloose</span> - Teen (Age 13&ndash;17)</div>`,
     perks: ["Badge", "Conbook", "Simple colored lanyard"],
-    price: "<span class='text-2xl'>$20</span>",
+    price: "<span class='text-xl xl:text-2xl'>$20</span>",
   },
   {
-    name: `<div class="text-2xl"><span class="italic">The Sound of Magic - 3 Day Pass</span></div>`,
+    name: `<div class="text-xl xl:text-2xl"><span class="italic">The Sound of Magic - 3 Day Pass</span></div>`,
     perks: ["Badge", "Conbook", "Simple colored lanyard"],
-    price: "<span class='text-2xl'>$65</span>",
+    price: "<span class='text-xl xl:text-2xl'>$65</span>",
   },
   {
-    name: `<div class="text-3xl"><span class="italic">My Fair Lyra</span></div>`,
+    name: `<div class="text-2xl xl:text-3xl"><span class="italic">My Fair Lyra</span></div>`,
     limit: 25,
     perks: ["All previous perks", "Access to VIP Meet and Greet"],
-    price: "<span class='text-3xl'>$350</span>",
+    price: "<span class='text-2xl xl:text-3xl'>$350</span>",
   },
   {
-    name: `<div class="text-3xl"><span class="italic">The Prohoovers</span></div>`,
+    name: `<div class="text-2xl xl:text-3xl"><span class="italic">The Prohoovers</span></div>`,
     limit: 10,
     perks: [
       "All previous perks",
@@ -29,10 +29,10 @@ export default [
       "Access to VIP Sponsor Dinner",
       "Access to the #general chat in the HarmonyCon Discord Server",
     ],
-    price: "<span class='text-3xl'>$650</span>",
+    price: "<span class='text-2xl xl:text-3xl'>$650</span>",
   },
   {
-    name: `<div class="text-4xl"><span class="italic">Into the Everfree</span></div>`,
+    name: `<div class="text-3xl xl:text-4xl"><span class="italic">Into the Everfree</span></div>`,
     limit: 5,
     perks: [
       "All previous perks",
@@ -43,28 +43,28 @@ export default [
       "Limo service to/from airport and to/from VIP Sponsor Dinner",
       "Dinner at the Crown Block Steakhouse Friday evening (Formal wear mandatory) with Guests of Honor opting in",
     ],
-    price: "<span class='text-4xl'>$2,500</span>",
+    price: "<span class='text-3xl xl:text-4xl'>$2,500</span>",
   },
   {
-    name: `<div class="text-4xl"><span class="italic">In the Zephyr Heights</span></div>`,
+    name: `<div class="text-3xl xl:text-4xl"><span class="italic">In the Zephyr Heights</span></div>`,
     limit: 2,
     perks: [
       "All previous perks",
       "Terrace suite (check in Thursday, check out Sunday)",
     ],
-    price: "<span class='text-4xl'>$4,000</span>",
+    price: "<span class='text-3xl xl:text-4xl'>$4,000</span>",
   },
   {
-    name: `<div class="text-4.5xl"><span class="italic">The Book of Harmony</span></div>`,
+    name: `<div class="text-3.5xl xl:text-4.5xl"><span class="italic">The Book of Harmony</span></div>`,
     limit: 2,
     perks: [
       "All previous perks",
       "Balcony suite (check in Thursday, check out Monday)",
     ],
-    price: "<span class='text-4.5xl'>$5,000</span>",
+    price: "<span class='text-3.5xl xl:text-4.5xl'>$5,000</span>",
   },
   {
-    name: `<div class="text-5xl"><span class="italic">Princess Celestia Superstar</span></div>`,
+    name: `<div class="text-4xl xl:text-5xl"><span class="italic">Princess Celestia Superstar</span></div>`,
     limit: 1,
     perks: [
       `All previous perks (<span class="italic">The Prohoovers</span>)`,
@@ -75,7 +75,7 @@ export default [
       "Voice call with chairs to discuss what you want",
       "Singular Exclusive Badge Ribbon",
     ],
-    price: "<span class='text-5xl'>$20,000</span>",
+    price: "<span class='text-4xl xl:text-5xl'>$20,000</span>",
     style: `  background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
           -webkit-background-clip: text;
           -webkit-text-fill-color: transparent; text-shadow: 1px 1px 1px #ffffff55;`,
